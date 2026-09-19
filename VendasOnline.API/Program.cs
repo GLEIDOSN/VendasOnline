c56c6b456cc11dc68ad04ae6ffd86a72d6d94ac5
Gleidson Freitas <fc_gleidson@hotmail.com>
2026-09-19 20:08:08 -0300

    Primeiro commit: - Criação do projeto conforme template gerado pelo Visual Studio

---
+++ VendasOnline.API/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "VendasOnline.Api",
+        Version = "v1",
+        Description = "API REST de integração de dados para parceiros de negócios"
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "VendasOnline API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

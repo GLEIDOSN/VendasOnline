6715579bdadc0298f59961d8f795a263bc9dc371
Gleidson Freitas <fc_gleidson@hotmail.com>
2026-09-19 20:47:44 -0300

    Criação das classes models da aplicação

---
+++ VendasOnline.API/Enums/StatusPedido.cs
@@ -0,0 +1,9 @@
+﻿namespace VendasOnline.API.Enums;
+
+public enum StatusPedido
+{
+    Pendente = 1,
+    Processando = 2,
+    Concluido = 3,
+    Cancelado = 4
+}
---
+++ VendasOnline.API/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VendasOnline.API.Models;
+
+[Table("Produtos")]
+public class Produto
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "A descrição do produto é obrigatória.")]
+    [StringLength(150)]
+    public string? Descricao { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
+    [Range(0.01, 999999.99, ErrorMessage = "O preço deve ser maior que zero.")]
+    public decimal Preco { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue, ErrorMessage = "A quantidade em estoque não pode ser negativa.")]
+    public int QuantidadeEstoque { get; set; }
+
+    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+
+    public bool Ativo { get; set; } = true;
+}
